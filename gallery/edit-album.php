4708a877509e9a2d64efe4bf54ea02361950df18
Ryan Feb <118648924+subjectcode@users.noreply.github.com>
2024-02-17 12:43:46 +0700

    Add files via upload

---
+++ gallery/edit-album.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['UserID'])){
+        header("location:login");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Edit Album</title>
+</head>
+<body>
+    <h1>Halaman Edit Album</h1>
+    <p>Selamat datang <b><?=$_SESSION['NamaLengkap']?></b></p>
+    
+  <?php
+    include "navbar.php";
+  ?>
+
+    <form action="update-album.php" method="post">
+        <?php
+            include "connect/koneksi.php";
+            $AlbumID=$_GET['AlbumID'];
+            $sql=mysqli_query($koneksi,"SELECT * from album where AlbumID='$AlbumID'");
+            while($data=mysqli_fetch_array($sql)){
+        ?>
+        <input type="text" name="AlbumID" value="<?=$data['AlbumID']?>" hidden>
+        <table>
+            <tr>
+                <td>Nama Album</td>
+                <td><input type="text" name="NamaAlbum" value="<?=$data['NamaAlbum']?>"></td>
+            </tr>
+            <tr>
+                <td>Deskripsi</td>
+                <td><input type="text" name="Deskripsi" value="<?=$data['Deskripsi']?>"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="Ubah"></td>
+            </tr>
+        </table>
+        <?php
+            }
+        ?>
+    </form>
+
+    
+</body>
+</html>
